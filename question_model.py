bf61b428c66c90f2b59378d012ce9c1b7155c7d1
Ahmed Farah <Ahmed.Farah@healthstream.com>
2022-08-13 05:59:48 -0500

    qu

---
+++ question_model.py
@@ -0,0 +1,44 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+new_question = Question("what's", "False")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Users:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+
+# user_name = input("what is your name? ")
+# user_age = input("what is your age? ")
+#
+# followers = Users(user_name, user_age)
+#
+# print(f"Thanks {user_name}, your age is {user_age}")
+# print(followers.name, followers.age)
+# print(f"Thanks {followers.name}, your age is {followers.age}")
